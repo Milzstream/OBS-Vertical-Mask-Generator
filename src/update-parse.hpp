269691c72388aa13e5396aab638b1369ea0011bd
Milz <elliottquick@live.com>
2026-09-18 14:02:45 -0400

    Fix update checker opening GitHub without SSL. (#36)
    Download now upgrades http:// and schemeless GitHub links to https://,
    refuses other hosts, and opens the URL only if the scheme is https.

--- src/update-parse.hpp
+++ src/update-parse.hpp
@@ -104,3 +104,82 @@ inline void mask_find_windows_asset(const std::string &json, std::string &url)
 	}
 	url = !exe.empty() ? exe : zip;
 }
+
+inline void mask_json_unescape_solidus(std::string &s)
+{
+	size_t w = 0;
+	for (size_t r = 0; r < s.size(); r++) {
+		if (s[r] == '\\' && r + 1 < s.size() && s[r + 1] == '/') {
+			s[w++] = '/';
+			r++;
+		} else {
+			s[w++] = s[r];
+		}
+	}
+	s.resize(w);
+}
+
+inline bool mask_https_github_host(const std::string &url)
+{
+	const char *pfx = "https://";
+	const size_t pfx_len = 8;
+	if (url.size() <= pfx_len || url.compare(0, pfx_len, pfx) != 0)
+		return false;
+
+	size_t start = pfx_len;
+	const size_t slash = url.find('/', start);
+	const size_t at = url.find('@', start);
+	if (at != std::string::npos && (slash == std::string::npos || at < slash))
+		start = at + 1;
+
+	size_t end = url.find_first_of("/?#:", start);
+	if (end == std::string::npos)
+		end = url.size();
+	if (end <= start)
+		return false;
+
+	std::string host = url.substr(start, end - start);
+	for (char &c : host) {
+		if (c >= 'A' && c <= 'Z')
+			c = static_cast<char>(c - 'A' + 'a');
+	}
+	if (host == "github.com" || host == "www.github.com")
+		return true;
+	const char *suffix = ".githubusercontent.com";
+	const size_t suffix_len = std::strlen(suffix);
+	return host.size() > suffix_len && host.compare(host.size() - suffix_len, suffix_len, suffix) == 0;
+}
+
+inline bool mask_normalize_https_url(std::string &url)
+{
+	if (url.empty())
+		return false;
+
+	mask_json_unescape_solidus(url);
+
+	size_t a = 0;
+	size_t b = url.size();
+	while (a < b && (url[a] == ' ' || url[a] == '\t' || url[a] == '\r' || url[a] == '\n'))
+		a++;
+	while (b > a && (url[b - 1] == ' ' || url[b - 1] == '\t' || url[b - 1] == '\r' || url[b - 1] == '\n'))
+		b--;
+	url = url.substr(a, b - a);
+	if (url.empty())
+		return false;
+
+	if (url.size() >= 7 && (url.compare(0, 7, "http://") == 0 || url.compare(0, 7, "HTTP://") == 0))
+		url.replace(0, 7, "https://");
+	else if (url.compare(0, 2, "//") == 0)
+		url.insert(0, "https:");
+	else if (url.compare(0, 8, "https://") != 0 && url.compare(0, 8, "HTTPS://") != 0)
+		url.insert(0, "https://");
+
+	if (url.compare(0, 8, "HTTPS://") == 0)
+		url.replace(0, 8, "https://");
+
+	if (!mask_https_github_host(url)) {
+		url.clear();
+		return false;
+	}
+	return true;
+}
